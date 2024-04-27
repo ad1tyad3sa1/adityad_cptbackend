919db860b0a81e4e8b18984df364aee2220077a2
ad1tyad3sa1 <aditya7desai@gmail.com>
2024-04-27 12:39:00 -0700

    comments

--- api/baking.py
+++ api/baking.py
@@ -1,38 +1,60 @@
-import json, jwt
+# Import necessary libraries and modules
+import json
+import jwt
 from flask import Blueprint, request, jsonify, current_app, Response
-from flask_restful import Api, Resource # used for REST API building
+from flask_restful import Api, Resource  # Used for REST API building
 from datetime import datetime
 from auth_middleware import token_required
 
+# Import the Baking model
 from model.bakings import Baking
 
-baking_api = Blueprint('baking_api', __name__,
-                   url_prefix='/api/baking')
+# Create a Blueprint for the baking API with a specific URL prefix
+baking_api = Blueprint('baking_api', __name__, url_prefix='/api/baking')
 
-# API docs https://flask-restful.readthedocs.io/en/latest/api.html
+# Initialize Flask-Restful API with the Blueprint
 api = Api(baking_api)
 
+# Define a class for handling Baking API operations
 class BakingAPI:        
-    class _CRUD(Resource):  # User API operation for Create, Read.  THe Update, Delete methods need to be implemeented
-        def post(self): # Create method
-            ''' Read data for json body '''
+    # Nested class for CRUD operations on Baking resources
+    class _CRUD(Resource):  
+        # HTTP POST method for creating a new Baking resource
+        def post(self): 
+            ''' 
+            Create a new Baking resource
+            '''
+            # Read data from the JSON request body
             body = request.get_json()
             
-            ''' Avoid garbage in, error checking '''
-            # validate name
-            recpie = body.get('recpie')
-            uo = Baking(recpie=recpie)
+            # Extract recipe information from the request body
+            recipe = body.get('recipe')
+            
+            # Create a new Baking instance with the provided recipe
+            baking_object = Baking(recipe=recipe)
         
-            user = uo.create()
-            # success returns json of user
-            if user:
-                return jsonify(user.read())
-            # failure returns error
-            return {'message': f'Processed {recpie}, either a format error or User ID {recpie} is duplicate'}, 400
-        def get(self): # Read Method
-            users = Baking.query.all()    # read/extract all users from database
-            json_ready = [user.read() for user in users]  # prepare output in json
-            return jsonify(json_ready)  # jsonify creates Flask response object, more specific to APIs than json.dumps
- 
-    # building RESTapi endpoint
+            # Attempt to create the Baking resource in the database
+            baked_item = baking_object.create()
+            
+            # If creation is successful, return the JSON representation of the created resource
+            if baked_item:
+                return jsonify(baked_item.read())
+            # If creation fails, return an error message
+            return {'message': f'Failed to process {recipe}. Either a format error occurred or the recipe is a duplicate.'}, 400
+        
+        # HTTP GET method for retrieving all Baking resources
+        def get(self): 
+            ''' 
+            Retrieve all Baking resources
+            '''
+            # Query all Baking resources from the database
+            bakings = Baking.query.all()
+            
+            # Convert each Baking resource to JSON format
+            json_ready = [baking.read() for baking in bakings]
+            
+            # Return the JSON representation of all Baking resources
+            return jsonify(json_ready)
+    
+    # Add CRUD resource to the REST API endpoint
     api.add_resource(_CRUD, '/')
